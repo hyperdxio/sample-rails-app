ab25a78615bb5c04e2a6ea66100df28eed1ad3c0
Warren <5959690+wrn14897@users.noreply.github.com>
2023-05-16 13:47:27 -0700

    feat: add dice ctrl

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  get 'rolldice', to: 'dice#roll'
 
   # You can have the root of your site routed with "root"
-
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

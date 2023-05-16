4f4d7a783bfc42bb25573547f3b28a4b3548577d
Warren <5959690+wrn14897@users.noreply.github.com>
2023-05-16 14:20:19 -0700

    feat: config otel

---
+++ config/initializers/hyperdx.rb
@@ -0,0 +1,23 @@
+require 'opentelemetry-exporter-otlp'
+require 'opentelemetry/instrumentation/all'
+require 'opentelemetry/sdk'
+
+OpenTelemetry::SDK.configure do |c|
+  c.use_all() # enables all instrumentation!
+end
+
+Rails.application.configure do
+  Rails.logger = Logger.new(STDOUT)
+  Rails.logger.formatter = proc do |severity, time, progname, msg|
+    span_id = OpenTelemetry::Trace.current_span.context.hex_span_id
+    trace_id = OpenTelemetry::Trace.current_span.context.hex_trace_id
+    if defined? OpenTelemetry::Trace.current_span.name
+      operation = OpenTelemetry::Trace.current_span.name
+    else
+      operation = 'undefined'
+    end
+
+    # config.logger.info(OpenTelemetry::Trace.current_span.context.hex_span_id)
+    "#{time}, #{severity}: #{msg} - trace_id=#{trace_id} - span_id=#{span_id} - operation=#{operation}\n"
+  end
+end

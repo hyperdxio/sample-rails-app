8d6b85e78bc545d9d1d241b70218ca7f42c6799d
Mike Shi <hi@michaelshi.me>
2023-05-17 14:40:50 -0700

    print rolled number

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,7 +1,29 @@
 class DiceController < ApplicationController
   def roll
-    render json: (rand(6) + 1).to_s
+    rolled_number = rand(6) + 1
+    render json: rolled_number.to_s
 
-    Rails.logger.info "Rolled a die 🐱🐱🐱🐱🐱🐱🐱🐱"
+    Rails.logger.info "Rolled a die 🐱: #{rendered_die(rolled_number)} (#{rolled_number})"
+  end
+
+  private
+
+  def rendered_die(number)
+    case number
+    when 1
+      '⚀'
+    when 2
+      '⚁'
+    when 3
+      '⚂'
+    when 4
+      '⚃'
+    when 5
+      '⚄'
+    when 6
+      '⚅'
+    else
+      'Unknown'
+    end
   end
 end
